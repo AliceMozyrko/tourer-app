18026b9c721b024bac473ae9ece1f02666eb6c82
AliceMozyrko <mozyrkoalisa@gmail.com>
2025-08-26 15:07:21 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import Hero from "../../components/Hero/Hero"
 import AboutUs from "../../components/AboutUs/AboutUs"
 import OurCar from "../../components/OurCar/OurCar"
-import Services from "../../Components/Services/Services"
+import Services from "../../components/Services/Services"
 import Footer from "../../components/Footer/Footer"
 import css from "./HomePage.module.css"
 
