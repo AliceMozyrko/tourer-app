bb11c979b56ae6c8d4202b7d1957a73b492b9497
AliceMozyrko <mozyrkoalisa@gmail.com>
2025-08-26 16:12:03 +0300

    footer icons transition

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,11 +23,11 @@ const Footer = () => {
       </NavLink>
       <p className={css.txt}>© 2025 Tourer. All rights reserved.</p>
       <div className={css.socials}>
-        <FaWhatsapp size={30} color="#25D366" onClick={handleClick} />
-        <FaViber size={30} color="#665CAC" onClick={handleClick} />
-        <FaTelegram size={30} color="#0088cc" onClick={handleClick} />
-        <SiSignal size={30} color="#3A76F0" onClick={handleClick} />
-        <SiWechat size={30} color="#7BB32E" onClick={handleClick} />
+        <a onClick={handleClick}><FaWhatsapp size={30} color="#25D366" /></a>
+        <a onClick={handleClick}><FaViber size={30} color="#665CAC" /></a>
+        <a onClick={handleClick}><FaTelegram size={30} color="#0088cc" /></a>
+        <a onClick={handleClick}><SiSignal size={30} color="#3A76F0" /></a>
+        <a onClick={handleClick}><SiWechat size={30} color="#7BB32E" /></a>
       </div>
     </div>
   )
