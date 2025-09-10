89ea478e8ec1eaade990dc1217a78ce44e95a4b7
AliceMozyrko <mozyrkoalisa@gmail.com>
2025-09-10 09:03:54 +0300

    update path ourcar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import Hero from "../../components/Hero/Hero"
 import AboutUs from "../../components/AboutUs/AboutUs"
-import OurCar from "../../Components/OurCar/OurCar"
-import Services from "../../Components/Services/Services"
+import OurCar from "../../components/OurCar/OurCar"
+import Services from "../../components/Services/Services"
 import Footer from "../../components/Footer/Footer"
 import css from "./HomePage.module.css"
 
